e6149cf409630923650f804028f02b5a98da4ac3
Shashank Singh <singhshashankcic@ducic.ac.in>
2019-07-21 22:41:30 +0530

    Fixing error

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-var _ = require('lodash');
+// var _ = require('lodash');
 
-var array = [1,2,3,4,5,6,7,8];
-console.log('answer:', _.without(array, 3));
+// var array = [1,2,3,4,5,6,7,8];
+// console.log('answer:', _.without(array, 3));
 
 var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
